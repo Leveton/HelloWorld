bc31b8df8c3f69ac4a97bf6b18d90ab1a9dfd9b5
Leveton <mleveton@gmail.com>
2016-04-12 14:37:17 -0400

    SSSearchBar added

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "SSSearchBar.h"
 
-@interface ViewController ()
+@interface ViewController ()<SSSearchBarDelegate>
 @property (nonatomic, strong) UILabel *label;
+@property (strong, nonatomic) SSSearchBar *searchBar;
 @end
 
 @implementation ViewController
@@ -17,9 +19,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //    self.searchBar.cancelButtonHidden = NO;
+    //    self.searchBar.placeholder = NSLocalizedString(@"Search text here!", nil);
+    //    self.searchBar.delegate = self;
+    //    [self.searchBar becomeFirstResponder];
+    //
+    //    self.data = @[ @"Hey there!", @"This is a custom UISearchBar.", @"And it's really easy to use...", @"Sweet!" ];
+    //    self.searchData = self.data;
+    
+    [[self searchBar] becomeFirstResponder];
+    
 }
 
-//test
 - (void)viewWillLayoutSubviews{
     [super viewWillLayoutSubviews];
     
@@ -28,6 +39,15 @@
     labelFrame.size.height = 60.0f;
     labelFrame.size.width = CGRectGetWidth([[self view] frame]);
     [[self label] setFrame:labelFrame];
+    
+    CGRect searchFrame = [[self searchBar] frame];
+    searchFrame.size.height = 32.0f;
+    searchFrame.size.width = 300.0f;
+    searchFrame.origin.y = 90.0f;
+    searchFrame.origin.x = [self horizontallyCenteredFrameForChildFrame:searchFrame].origin.x;
+    [[self searchBar] setFrame:searchFrame];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,7 +58,7 @@
 - (UILabel *)label{
     if (!_label){
         _label = [[UILabel alloc] initWithFrame:CGRectZero];
-        [_label setText:@"Search"];
+        [_label setText:@"Present View Controller"];
         [_label setTextAlignment:NSTextAlignmentCenter];
         [[self view] addSubview:_label];
         return _label;
@@ -46,5 +66,26 @@
     return _label;
 }
 
+- (SSSearchBar *)searchBar{
+    if (!_searchBar){
+        _searchBar = [[SSSearchBar alloc] initWithFrame:CGRectZero];
+        [_searchBar setDelegate:self];
+        [_searchBar setPlaceholder:@"mike"];
+        [_searchBar setBackgroundColor:[UIColor blueColor]];
+        [[self view] addSubview:_searchBar];
+        return _searchBar;
+    }
+    return _searchBar;
+}
+
+- (CGRect)horizontallyCenteredFrameForChildFrame:(CGRect)childRect{
+    CGRect viewBounds = [[self view] bounds];
+    CGFloat listMinX = CGRectGetMidX(viewBounds) - (CGRectGetWidth(childRect)/2);
+    CGRect newChildFrame = CGRectMake(listMinX,
+                                      CGRectGetMinY(childRect),
+                                      CGRectGetWidth(childRect),
+                                      CGRectGetHeight(childRect));
+    return CGRectIntegral(newChildFrame);
+}
 
 @end
